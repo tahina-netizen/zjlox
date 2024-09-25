9f969b2d030d62316765c3a3bc499ada1582df55
Zo Tahina <zratefiarivony@gmail.com>
2024-09-25 15:32:21 +0200

    Ch5: representing code

---
+++ answers.md
@@ -0,0 +1,56 @@
+## Chapter 5: Representing Code
+
+### 1
+```text
+expr → expr ( "(" ( expr ( "," expr )* )? ")" | "." IDENTIFIER )+
+     | IDENTIFIER
+     | NUMBER
+```
+
+could be rewritten as
+
+```text
+expr → expr ("(" ( expr ( "," expr )* )? ")")+
+
+append_arg -> "," expr
+append_many_args -> ""
+append_many_args -> append_arg append_many_args
+arg_list → ""
+arg_list → expr
+arg_list -> expr append_many_args
+apply_args -> "(" arg_list ")"
+
+access_attribute → "." IDENTIFIER
+maybe_access_attribute -> ""
+maybe_access_attribute -> access_attribute
+many_access_attribute -> ""
+many_access_attribute -> access_attribute many_access_attribute
+expr → expr access_attribute many_access_attribute
+
+expr → expr
+
+expr → IDENTIFIER
+
+expr → NUMBER
+```
+
+This bit of grammar encode 3 kind of expression:
+- accessing an attribute of an object or,
+- an identifier or,
+- a number
+
+### 2
+
+```hs
+data Expr = Binary Expr Token Expr | Grouping Expr | Unary Token Expr | Literal String
+
+prettyPrint :: Expr -> String
+prettyPrint (Binary left op right) = undefined
+prettyPrint (Grouping expr) = undefined
+prettyPrint (Unary op expr) = undefined
+prettyPrint (Literal val) = undefined
+```
+
+### 3
+
+See `RPNPrinter.java`
---
+++ src/main/java/com/tahinanetizen/zjlox/RPNPrinter.java
@@ -0,0 +1,40 @@
+package com.tahinanetizen.zjlox;
+
+import com.tahinanetizen.zjlox.Expr.Binary;
+import com.tahinanetizen.zjlox.Expr.Grouping;
+import com.tahinanetizen.zjlox.Expr.Literal;
+import com.tahinanetizen.zjlox.Expr.Unary;
+
+public class RPNPrinter implements Expr.Visitor<String> {
+
+    public static void main(String[] args) {
+        Expr expr = new Expr.Binary(
+            new Expr.Grouping(new Expr.Binary(new Expr.Literal(1), new Token(TokenType.PLUS, "+", null, 1), new Expr.Literal(2))), 
+            new Token(TokenType.STAR, "*", null, 1), 
+            new Expr.Grouping(new Expr.Binary(new Expr.Literal(4), new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(3)))
+            );
+
+        System.out.println(expr.accept(new RPNPrinter()));
+    }
+
+    @Override
+    public String visitBinaryExpr(Binary expr) {
+        return expr.left.accept(this) + " " + expr.right.accept(this) + " " + expr.operator.lexeme;
+    }
+
+    @Override
+    public String visitGroupingExpr(Grouping expr) {
+       return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Literal expr) {
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Unary expr) {
+        return expr.right.accept(this);
+    }
+    
+}
